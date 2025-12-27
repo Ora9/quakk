957beedb748027b592617d06e8a607db41b48ceb
Ora9 <ora9@duck.com>
2025-12-27 16:36:56 +0100

    style: fmt

--- src/graph/node.rs
+++ src/graph/node.rs
@@ -17,13 +17,11 @@ pub trait Node: Debug {
     /// The node "title" when displayed
     fn title(&self) -> &str;
 
-
     fn evaluate(&self, out_id: InoutId, lasy_executor: LasyExecutor, meta: Meta);
 
     fn node_inout_id_for(&self, inout_name: &str, node_id: NodeId) -> Option<NodeInoutId> {
-        self.id_for(inout_name).and_then(|inout_id| {
-            Some(NodeInoutId::new(node_id, inout_id))
-        })
+        self.id_for(inout_name)
+            .and_then(|inout_id| Some(NodeInoutId::new(node_id, inout_id)))
     }
 
     fn id_for(&self, inout_name: &str) -> Option<InoutId>;
