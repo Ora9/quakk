ac43fa4712f4b61c023f4869b201ad6afa78b8ee
Ora9 <ora9@duck.com>
2025-12-27 23:21:50 +0100

    feat: add a simple new node (Add)

--- packages/numeric/src/lib.rs
+++ packages/numeric/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Mul;
+use std::ops::{Add as opsAdd, Mul as opsMul};
 
 use quack_sth::{InoutId, LasyFold, Meta, Node};
 
@@ -60,6 +60,37 @@ impl Node for Multiply {
 
     fn title(&self) -> &str {
         "Multiply"
+    }
+
+    fn id_for(&self, inout_name: &str) -> Option<InoutId> {
+        match inout_name {
+            "term1" | "term2" => Some(InoutId::new_in_from(inout_name)),
+            "out" => Some(InoutId::new_in_from(inout_name)),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Add;
+
+impl Node for Add {
+    fn new() -> Self {
+        Self
+    }
+
+    fn fold(&self, _out_id: InoutId, lasy_fold: LasyFold, meta: Meta) -> f32 {
+        if let Some(term1) = lasy_fold.get_input(self.id_for("term1").unwrap(), meta)
+            && let Some(term2) = lasy_fold.get_input(self.id_for("term2").unwrap(), meta)
+        {
+            term1.add(term2)
+        } else {
+            0.0
+        }
+    }
+
+    fn title(&self) -> &str {
+        "Add"
     }
 
     fn id_for(&self, inout_name: &str) -> Option<InoutId> {
--- packages/quack_sth_cli/src/main.rs
+++ packages/quack_sth_cli/src/main.rs
@@ -10,7 +10,10 @@ fn main() {
 
         let number_a = graph.insert(Box::new(Number::new()));
         let number_b = graph.insert(Box::new(Number::new()));
+        let number_c = graph.insert(Box::new(Number::new()));
+
         let mult = graph.insert(Box::new(Multiply::new()));
+        let add = graph.insert(Box::new(Add::new()));
 
         let _ = graph.patch(
             number_a.id_for("out").unwrap(),
@@ -20,10 +23,15 @@ fn main() {
             number_b.id_for("out").unwrap(),
             mult.id_for("term2").unwrap(),
         );
+        let _ = graph.patch(mult.id_for("out").unwrap(), add.id_for("term1").unwrap());
+        let _ = graph.patch(
+            number_c.id_for("out").unwrap(),
+            add.id_for("term2").unwrap(),
+        );
 
-        let numout = graph.graph_out_id_for("number_out").unwrap();
+        let num_out = graph.graph_out_id_for("number_out").unwrap();
 
-        let _ = graph.patch(mult.id_for("out").unwrap(), numout);
+        let _ = graph.patch(add.id_for("out").unwrap(), num_out);
     }
 
     dbg!(qk.evaluate_for("number_out").unwrap());
