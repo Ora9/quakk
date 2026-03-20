90dfba20db3d8e27b220ec0d58eb53f8364ed6dc
Ora9 <ora9@duck.com>
2026-03-20 16:12:11 +0100

    quakk_app: and git add lib.rs, it usally work better like that

---
+++ crates/quakk_app/src/lib.rs
@@ -0,0 +1,14 @@
+mod app;
+pub use app::App;
+
+mod app_state;
+pub use app_state::AppState;
+
+mod command;
+pub use command::Command;
+
+mod keybinding;
+pub use keybinding::{Keybind, Modifiers};
+
+mod components;
+pub use components::menu::{Menu, MenuAnchor, MenuEntry};
